48f4424a9cae958e3038215f84b67ef4d9269355
RubenCastroCoding <RubenCastroCoding@gmail.com>
2022-06-11 12:55:40 -0400

    add sale option

---
+++ public/java/add-sale.js
@@ -0,0 +1,36 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const picture = document.querySelector('#picture').value;
+    const item_name = document.querySelector('#item_name').value;
+    const price = document.querySelector('#price').value;
+    const quantity = document.querySelector('#quantity').value;
+    const status = document.querySelector('#is_sold:checked') ? true : false;
+    const description = document.querySelector('#description').value;
+    
+    const response = await fetch(`/api/profile/:id`, {
+      method: 'POST',
+      body: JSON.stringify({
+        picture,
+        item_name,
+        price,
+        quantity,
+        status,
+        description
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to add item');
+    }
+  }
+  
+  document
+    .querySelector('.new-item-form')
+    .addEventListener('submit', newFormHandler);
+  
