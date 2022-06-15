33a7e66412c3c97680c25f6969c7073d6913fcd1
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-06-15 18:59:46 -0400

    added edit hb route and got it to send empty array

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,20 +8,21 @@ router.get('/', async (req, res) => {
 //redirects to / above
 router.get('/homepage', async (req, res) => {
   //res.render('homepage');
-  res.redirect('/')
+  res.redirect('/');
 });
 
 router.get('/add-item/:id', async (req, res) => {
-  console.log("add item id",req.params.id)
+  console.log('add item id', req.params.id);
   const profileId = req.params.id;
-  res.render('add-item', {profile:profileId})
-})
+  res.render('add-item', { profile: profileId });
+});
+
 //need to send id to the view
 router.post('/save-new-item', async (req, res) => {
-  console.log(req.body)
-  console.log("post in id", req.params.id)
+  console.log(req.body);
+  console.log('post in id', req.params.id);
   const id = req.body.profileId;
-  res.redirect(`/profile/${id}`)
+  res.redirect(`/profile/${id}`);
   //send data to db
   // try {
   //   const newPost = await SaleItem.create({
@@ -36,6 +37,26 @@ router.post('/save-new-item', async (req, res) => {
   //   res.status(400).json(err);
   // }
 });
+
+router.get('/edit-item/:id', async (req, res) => {
+  try {
+    const itemId = await itemId.findByPk(req.params.id, {
+      include: [
+        {
+          model: SaleItem,
+        },
+        {
+          model: UserBio,
+        },
+      ],
+    })
+    const itemData = itemId.get({ plain: true})
+    res.render('edit-item', itemData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 router.get('/profile/:id', async (req, res) => {
   console.log('/profile/1');
